9640670850fcd0cd5425b3b820d87f2acb0a5c9c
sdsuji <sujitacbe2018@gmail.com>
2025-07-10 12:04:55 +0530

    verification email sending logic changed

--- user-service/utils/sendEmail.js
+++ user-service/utils/sendEmail.js
@@ -2,12 +2,15 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (to, subject, html) => {
   const transporter = nodemailer.createTransport({
-    service: 'Gmail', 
-    auth: {
-      user: process.env.EMAIL_USER,     
-      pass: process.env.EMAIL_PASS      
-    }
-  });
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // use SSL
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
 
   await transporter.sendMail({
     from: `"MetaStudy" <${process.env.EMAIL_USER}>`,
